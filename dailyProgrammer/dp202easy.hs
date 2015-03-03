709b7dc081198c3b361dc57d7970672ac8d5636c
Thomas Dietert <tdietert@mail.usf.edu>
2015-03-03 10:36:21 -0500

    fixed indentation

--- dailyProgrammer/dp202easy.hs
+++ dailyProgrammer/dp202easy.hs
@@ -1,14 +1,14 @@
-	import Control.Applicative
-	import Data.List.Split
-	import System.Environment
-	import Data.Char
+import Control.Applicative
+import Data.List.Split
+import System.Environment
+import Data.Char
 
-	-- I'm so stupid I had to look at a spoiler to know it was 8-bit ascii
-	ascii :: String -> String
-	ascii = map (chr . raiseDigits) . chunksOf 8 . map digitToInt
-	    where raiseDigits x = sum . getZipList $ ((*) . (2^)) <$> ZipList [7,6..] <*> ZipList x
+-- I'm so stupid I had to look at a spoiler to know it was 8-bit ascii
+ascii :: String -> String
+ascii = map (chr . raiseDigits) . chunksOf 8 . map digitToInt
+    where raiseDigits x = sum . getZipList $ ((*) . (2^)) <$> ZipList [7,6..] <*> ZipList x
 
-	main = do
-		(input:args) <- getArgs
-		print $ ascii input
+main = do
+	(input:args) <- getArgs
+	print $ ascii input
 
